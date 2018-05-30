5582b94c9fb89b943304686e2c7d9047b5f480de
nookeey <eernestt@o2.pl>
2018-05-30 11:43:19 +0200

    commit_l26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
+import { NowyComponent } from './nowy/nowy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    Test2Component
+    Test2Component,
+    NowyComponent
   ],
   imports: [
     BrowserModule
