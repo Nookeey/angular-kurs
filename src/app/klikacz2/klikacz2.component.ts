27cb33339f24e44c4802ebe9a47a0200bd7a2d09
Nookeey <eernestt@o2.pl>
2018-06-05 21:00:05 +0200

    commit_Serwisy_w_Angularze_wstrzykiwanie

--- src/app/klikacz2/klikacz2.component.ts
+++ src/app/klikacz2/klikacz2.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ClickService } from '../services/click.service';
 
 @Component({
   selector: 'app-klikacz2',
   templateUrl: './klikacz2.component.html',
-  styleUrls: ['./klikacz2.component.css']
+  styleUrls: ['./klikacz2.component.css'],
+  providers: [ClickService]
 })
 export class Klikacz2Component implements OnInit {
 
   click = 0;
 
-  constructor() { }
+  constructor(private clickService: ClickService) { }
 
   ngOnInit() {
   }
 
   add(): void {
     this.click += 1;
+    this.clickService.addClicks();
   }
 
 }
