2b897b9c5884be023db0e9b6c21364d89cff1a89
nookeey <eernestt@o2.pl>
2018-06-08 16:18:38 +0200

    commit_59_Dyrektywy_implementacja_dyrektyw

--- src/app/shared/checked.directive.ts
+++ src/app/shared/checked.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appChecked]'
 })
-export class CheckedDirective {
+export class CheckedDirective implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    const li = this.el.nativeElement;
+    this.renderer.setStyle(li, 'list-style-image', 'url(/assets/tick.png)');
+    this.renderer.setStyle(li, 'background', 'aquamarine');
+  }
 }
