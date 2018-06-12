4a5c5ee9b84d54b7c9de22caf03d8456cabfefdd
nookeey <eernestt@o2.pl>
2018-06-12 14:44:57 +0200

    commit_85_Aplikacja_Lista_zadan_wykorzystujemy_API_chmury_zapytanie_POST

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,13 @@ export class HttpService {
     this.http.get(this.URL_DB, {params: this.param})
     .subscribe(tasks => {
       console.log(tasks);
+    });
+  }
+
+  saveTasks(list: Array<Task>) {
+    this.http.post(this.URL_DB, list, {params: this.param})
+    .subscribe(data => {
+      console.log(data);
     });
   }
 }
