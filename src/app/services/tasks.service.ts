75b3ce9f689b30323da287dc162694c45910737e
nookeey <eernestt@o2.pl>
2018-06-12 14:11:09 +0200

    commit_82_Aplikacja_Lista_zadan_refaktoryzacjaTaskService

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -6,10 +6,8 @@ import { BehaviorSubject, Observable, } from 'rxjs';
 export class TasksService {
 
   private tasksList: Array<Task> = [];
-  private tasksDone: Array<Task> = [];
 
   private tasksListObs = new BehaviorSubject<Array<Task>>([]);
-  private tasksDoneObs = new BehaviorSubject<Array<Task>>([]);
 
   constructor() {
     this.tasksList =
@@ -17,7 +15,8 @@ export class TasksService {
       {name: 'Sprzatanie', created: new Date().toLocaleString(), isDone: false},
       {name: 'Nauka', created: new Date().toLocaleString(), isDone: false},
       {name: 'Podlewanie', created: new Date().toLocaleString(), isDone: false},
-      {name: 'Zakupy', created: new Date().toLocaleString(), isDone: false}
+      {name: 'Zakupy', created: new Date().toLocaleString(), isDone: false},
+      {name: 'Zakupy', created: new Date().toLocaleString(), end: new Date().toLocaleDateString(), isDone: true}
     ];
     this.tasksListObs.next(this.tasksList);
   }
@@ -33,17 +32,14 @@ export class TasksService {
   }
 
   done(task: Task) {
-    this.tasksDone.push(task);
-    this.remove(task);
-    this.tasksDoneObs.next(this.tasksDone);
+    task.end = new Date().toLocaleString();
+    task.isDone = true;
+    const list = this.tasksListObs.getValue();
+    this.tasksListObs.next(list);
   }
 
   getTasksListObs(): Observable<Array<Task>> {
     return this.tasksListObs.asObservable();
-  }
-
-  getTasksDoneObs(): Observable<Array<Task>> {
-    return this.tasksDoneObs.asObservable();
   }
 
 }
