202cd527e8412772fc53ace87ae243e4edf12869
nookeey <eernestt@o2.pl>
2018-06-23 21:55:19 +0200

    commit_142_146_Projekt_Lista_Zadan_implementacja_formularza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { TasksService } from './services/tasks.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
